d4b6a9962b84e41f643c9d611d1dbaed1634c427
Gus Heptig <gusheptig@gmail.com>
2021-05-13 11:27:38 -0400

    added basic navbar with correct router links

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="navbar">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item
+            active-class="active"
+            class="nav-link"
+            v-for="routes in links"
+            :key="routes.id"
+            :to="routes.path"
+          >
+            {{ routes.name }}
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          name: "Search",
+          path: "/",
+        },
+        {
+          id: 1,
+          name: "Saved",
+          path: "/saved",
+        },
+      ],
+    };
+  },
+};
+</script>
